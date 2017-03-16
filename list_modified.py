1dee39477af2877aea871715420e8bf0a87f8006
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2017-03-15 18:06:26 -0600

    Some improvements

--- list_modified.py
+++ list_modified.py
@@ -1,7 +1,7 @@
 ﻿import boto3
-import logging
+import datetime
 import iso8601
-from datetime import datetime
+import logging
 
 logger = logging.getLogger('list_modified')
 formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
@@ -10,7 +10,8 @@ fileHandler.setFormatter(formatter)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(fileHandler)
 
-last_modified = iso8601.parse_date('2017-02-15 20:13:11+00:00')
+modified_since = iso8601.parse_date(str(datetime.datetime.utcnow() - datetime.timedelta(minutes=60)));
+logger.info('Fetching elements modified since:' + str(modified_since))
 version = '1-2'
 format = 'xml'
 
@@ -18,19 +19,18 @@ s3client = boto3.client('s3')
 
 bucket_s3_uri = 'orcid-public-data-dump-api-{}-{}-{}'
 
-def mark_as_modified(name, last_modified):
+def mark_as_modified(name, modified_since):
 	with open('modified.txt', 'a') as myfile:
-		myfile.write(name + ':' + last_modified + '\n')
+		myfile.write(name + ':' + modified_since + '\n')
 
 for i in range(0, 11):
 	checksum = str(i) if i != 10 else 'x'	
 	bucket_name = bucket_s3_uri.format(version, format, checksum)	
 	paginator = s3client.get_paginator('list_objects')
+	logger.info('Processing: ' + bucket_name)
 	page_iterator = paginator.paginate(Bucket=bucket_name)
 	for page in page_iterator:
 		for element in page['Contents']:
-			logger.info(str(element['Key']) + ': ' + str(element['LastModified']))
 			element_last_modified = iso8601.parse_date(str(element['LastModified']))
-			logger.info(str(element['LastModified']) + ' -> ' + str(element_last_modified))
-			if element_last_modified > last_modified:
+			if element_last_modified >= modified_since:
 				mark_as_modified(str(element['Key']), str(element_last_modified))
